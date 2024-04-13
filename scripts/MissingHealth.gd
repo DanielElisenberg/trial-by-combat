2c5f72c53a7e408cb36a2c31a2a2989dc68b33b4
leiver <leiv.rosseland@gmail.com>
2024-04-13 19:56:46 +0200

    Fix health bar

--- scripts/MissingHealth.gd
+++ scripts/MissingHealth.gd
@@ -7,10 +7,10 @@ func _draw():
 	var health_bar_rectangle: Rect2 = get_parent().get_rect()
 	var missing_health_rect
 	if flipped:
-		missing_health_rect = Rect2(Vector2(health_bar_rectangle.position.x + health_bar_rectangle.size.x * health_percentage, health_bar_rectangle.position.y), Vector2(health_bar_rectangle.size.x * (1 - health_percentage), health_bar_rectangle.size.y))
+		missing_health_rect = Rect2(Vector2(health_bar_rectangle.size.x * health_percentage, 0), Vector2(health_bar_rectangle.size.x * (1 - health_percentage), health_bar_rectangle.size.y))
 	else:
-		missing_health_rect = Rect2(health_bar_rectangle.position, Vector2(health_bar_rectangle.size.x * (1 - health_percentage), health_bar_rectangle.size.y))
-	draw_rect(missing_health_rect, Color.GRAY, true, 1.0)
+		missing_health_rect = Rect2(Vector2(0, 0), Vector2(health_bar_rectangle.size.x * (1 - health_percentage), health_bar_rectangle.size.y))
+	draw_rect(missing_health_rect, Color.BLACK, true, 1.0)
 
 func set_health_percentage(percentage):
 	health_percentage = percentage
