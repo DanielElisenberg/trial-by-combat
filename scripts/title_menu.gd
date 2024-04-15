3fbd589850bc7a37156f4d0c4040777a1f24a259
DanielElisenberg <Daniel@Elisenberg.com>
2024-04-15 20:42:50 +0200

    better title screen

--- scripts/title_menu.gd
+++ scripts/title_menu.gd
@@ -1,6 +1,25 @@
 extends Node2D
 
+@onready var selector = $Selector
+@onready var control_screen = $ControlScreen
+const controls_position = Vector2(964, 967)
+const play_position = Vector2(964, 833)
+var choice = "play"
 
 func _process(delta):
-	if Input.is_action_just_pressed("ui_accept"):
-		get_tree().change_scene_to_file("res://scenes/intro.tscn")
+	if control_screen.visible:
+		if Input.is_action_just_pressed("ui_accept"):
+			control_screen.visible = false
+	else:
+		if Input.is_action_just_pressed("down"):
+			choice = "controls"
+			selector.position = controls_position
+		if Input.is_action_just_pressed("jump"):
+			choice = "play"
+			selector.position = play_position
+		if Input.is_action_just_pressed("ui_accept"):
+			if choice == "play":
+				get_tree().change_scene_to_file("res://scenes/intro.tscn")
+			else:
+				control_screen.visible = true
+
