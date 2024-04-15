cdc1eb9c0001402cea7354d04087ca141bfdb647
leiver <leiv.rosseland@gmail.com>
2024-04-15 14:15:42 +0200

    line border for missing health

--- scripts/MissingHealth.gd
+++ scripts/MissingHealth.gd
@@ -15,6 +15,11 @@ func _draw():
 	else:
 		missing_health_rect = Rect2(Vector2(0, 0), Vector2(health_bar_rectangle.size.x * (1 - health_percentage), health_bar_rectangle.size.y))
 	draw_rect(missing_health_rect, Color.ORANGE_RED, true, 1.0)
+	if health_percentage < 1:
+		var x = health_bar_rectangle.size.x * health_percentage
+		if flipped:
+			x = health_bar_rectangle.size.x * (1 - health_percentage)
+		draw_line(Vector2(x, 0), Vector2(x, missing_health_rect.size.y), Color.FIREBRICK, 2, false)
 
 func set_health_percentage(percentage):
 	health_percentage = percentage
